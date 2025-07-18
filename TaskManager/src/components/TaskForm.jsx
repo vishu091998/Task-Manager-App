b3f673edc3d602725c67e0601acc169cf88b6978
vishal yadav <vishal.c.yadav@accenture.com>
2025-07-18 22:10:30 +0530

    latest backned changes

--- TaskManager/src/components/TaskForm.jsx
+++ TaskManager/src/components/TaskForm.jsx
@@ -1,16 +1,21 @@
 import { useContext, useState } from 'react';
 import { TaskContext } from '../context/TaskContext';
 import { RiAddCircleLine } from "react-icons/ri";
+import { FaCheck } from "react-icons/fa6";
+
 
 
 export default function TaskForm() {
-  const { addTask } = useContext(TaskContext);
+  const { addTask, tasks, setTasks } = useContext(TaskContext);
   const [text, setText] = useState('');
   const [error, setError] = useState('');
+  const [count , setCount] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const trimmed = text.trim();
+
+    console.log(e)
 
     if (!trimmed) {
       setError('Task cannot be empty.');
@@ -18,9 +23,30 @@ export default function TaskForm() {
     }
 
     addTask(trimmed);
+    handleCount(tasks.map((item)=> item.text))
     setText('');
     setError('');
   };
+
+  const handleCount = async(arr)=>{
+    const datatoSend = {
+      tasks : arr
+    }
+    const totalCount = await fetch('http://localhost:8081/add-task',{
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datatoSend)
+    });
+    const res = await totalCount.json();
+    setCount(res.totalCount);
+  }
+
+  const handleAllTaskComplete = () => {
+    const completedTasks = tasks.map((item) => ({ ...item, completed: true }));
+    setTasks(completedTasks)
+  }
 
   return (
     <>
@@ -35,11 +61,16 @@ export default function TaskForm() {
             placeholder="Add a new task..."
             className={`form-control ${error ? 'is-invalid' : ''}`}
           />
-          <button type="submit" className="btn btn-primary">
-            <RiAddCircleLine size={20}/>
+          <button type="submit" className="btn btn-primary" id='addBtn'>
+            <RiAddCircleLine size={20} />
           </button>
+          <button type='button' onClick={handleAllTaskComplete} className="btn btn-secondary" id='completeBtn'>
+            <FaCheck size={20} />
+          </button>
+          {count}
         </div>
       </form>
+
       {error && (
         <div className="text-danger" style={{ fontSize: '0.875rem' }}>
           {error}
